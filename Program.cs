a28352102cefce25cbe69e158923131a8b8ef034
Adachi <118907877+Adachi91@users.noreply.github.com>
2024-08-19 19:29:26 -0500

    Eternal Silence of the Sea
    What? Faded is still cool song.

---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using System.Globalization;
+using System.IO;
+
+//ConsoleColor holder = Console.ForegroundColor;
+
+//Console.ForegroundColor = ConsoleColor.Green;
+//Console.WriteLine();
+//Console.ForegroundColor = holder;
+print("***************************************\r\n    Screenshot Renamer by Adachi91\r\n***************************************", ConsoleColor.Green);
+
+Console.WriteLine("\r\nPlease enter the full path to the Directory where the screenshots are located (example: C:\\User\\MyUser\\Pictures\\Screenshots)." +
+    "\r\nAlternatively you can place the executable in the directory with the screenshots.\r\n>> At any point you can type quit to stop and exit the program.\r\n");
+
+string[] supportedExt = new string[] { ".png", ".jpg", ".jpeg", ".bmp", ".tga" };
+string[] accepted_inputs = new string[] { "y", "n", "no", "yes", "exit", "cancel", "stop", "q", "quit" };
+
+List<string> testDirectory = retrieveDirpyFiles(Environment.CurrentDirectory);
+
+
+foreach(string file in testDirectory) {
+    //Console.Write(Path.GetFileNameWithoutExtension(file).Length == "WoWScrnShot_120909_042440".Length);
+    Console.WriteLine(file);
+}
+
+if(testDirectory.Count > 0) {
+    Console.Write("Detected Screenshots. Proceede? (y/n) ");
+    string f;
+    while (true) {
+        f = Console.ReadLine().ToLower();
+
+        if (!string.IsNullOrWhiteSpace(f) && accepted_inputs.Contains(f))
+            break;
+        Console.Write("invalid input\r\nProceede? (y/n) ");
+    }
+
+    switch(f) {
+        case "no":
+        case "n":
+            Console.WriteLine("Proccess cancelled, exiting...");
+            Environment.Exit(0);
+        break;
+        case "yes":
+        case "y":
+            Console.WriteLine("Proceeding to fuck shit up"); //poolease change this
+        break;
+        case "exit":
+        case "stop":
+        case "cancel":
+        case "q":
+        case "quit":
+            Console.WriteLine("Exiting program.");
+        break;
+    }
+} else {
+    string f;
+
+    while(true) {
+        f = Console.ReadLine();
+
+        if(string.IsNullOrEmpty(f) || !Directory.Exists(f)) {
+            Console.WriteLine($"Could not find directory {f}.");
+        } else
+            break;
+    }
+}
+
+
+//WoWScrnShot_120909_042440.jpg
+//WoWScrnShot_082820_223514.avif IT'S REAL DON'T QUESTION IT, just believe in the power of AV1 encoding
+List<string> retrieveDirpyFiles(string path) {
+    return Directory.GetFiles(path).Where(
+        f => supportedExt.Contains(Path.GetExtension(f).ToLower())
+        && !File.GetAttributes(f).HasFlag(FileAttributes.ReparsePoint)
+        && !Path.GetFileName(f).EndsWith(".lnk", StringComparison.OrdinalIgnoreCase)
+        && Path.GetFileNameWithoutExtension(f).StartsWith("WoWScrnShot_", StringComparison.CurrentCultureIgnoreCase)
+        && Path.GetFileNameWithoutExtension(f).Length == 25
+    ).ToList();
+}
+
+
+void print(string txt, ConsoleColor c = ConsoleColor.Magenta) {
+    if (c == ConsoleColor.Magenta) {
+        Console.WriteLine(txt);
+        return;
+    }
+
+    ConsoleColor Neverevereverveverstop = Console.ForegroundColor; //Chvches - Never Say Die();
+    Console.ForegroundColor = c;
+    Console.WriteLine(txt);
+    Console.ForegroundColor = Neverevereverveverstop;
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# WoWScrnShot_Renamer
- Renames World of Warcraft screenshots to ISO 8601 for easy sorting
+# WoWScrnShot Renamer (World of Warcraft Screenshot Renamer)
+ World of Warcraft uses a Month-Day-Year format for older screenshots, which does not sort properly when sorting by name.  
+ This can be particularly problematic when the modification or creation date is altered, such as when files are copied. 
+ 
+ This program renames screenshots to the ISO 8601 format, allowing for proper chronological sorting when sorted by name.
+
+ ## Usage
+  You can launch and use the program from anywhere on your system as long as you provide the full path to your screenshot directory.
+  
+  Alternatively, you can place the executable inside the screenshot directory, and it will automatically detect the working directory.
+
+ ## Notice
+ While this program should not cause data loss, it is still possible in rare conditions.
+ 
+ **To prevent any issues, it is recommended to back up your files before renaming them**.
+ 
+ You can use tools such as zip (Windows), 7-Zip, or tar (for Unix-based system) to create an archive of your screenshots.
